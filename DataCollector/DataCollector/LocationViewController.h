aa6dde6f13ba3ee73c731350b7df09cba2a22359
Allen Shi <shidenghui@gmail.com>
2015-03-17 11:49:20 +0100

    add mapKit

---
+++ DataCollector/DataCollector/LocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationViewController.h
+//  DataCollector
+//
+//  Created by allen on 17/03/15.
+//  Copyright (c) 2015 allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationViewController : UIViewController
+
+@end
