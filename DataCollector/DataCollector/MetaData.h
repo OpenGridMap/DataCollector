e9337d1d53220aee2833493d3e3ae560c82a3fe2
Allen Shi <shidenghui@gmail.com>
2015-03-23 17:27:14 +0100

    add coreData

---
+++ DataCollector/DataCollector/MetaData.h
@@ -0,0 +1,27 @@
+//
+//  MetaData.h
+//  DataCollector
+//
+//  Created by allen on 23/03/15.
+//  Copyright (c) 2015 allen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class DataIterm;
+
+@interface MetaData : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * accuracy;
+@property (nonatomic, retain) NSString * address;
+@property (nonatomic, retain) NSString * deviceID;
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSNumber * metadataId;
+@property (nonatomic, retain) id tags;
+@property (nonatomic, retain) NSString * title;
+@property (nonatomic, retain) NSString * user;
+@property (nonatomic, retain) DataIterm *whichIterm;
+
+@end
